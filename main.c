5efe046f8e52f8388bd23252b8afa2bbd37ce077
Luiz Henrique Campos Barboza <luizhcb0@gmail.com>
2015-12-09 21:56:51 -0200

    Mudei o

--- main.c
+++ main.c
@@ -23,24 +23,5 @@ int main(){
     
     printf("%s\n",classHeap_ptr->className);
     
-    
-    Frame pFrameStack = (*Frame) malloc (20*sizeof(Frame));
-    pframeStack
-        sp=-1;
-        pClass=NULL;
-        pc=0;
-        stack=NULL;
-        pBaseFrame=NULL;
-        pOpStack=NULL;
-    }
-    
-    Frame(i2 sp)
-    {
-        this->sp=sp;
-        pClass=NULL;
-        pc=0;
-        stack=NULL;
-    }
-    
     return 0;
 }
